3beb8c8ab6f0514ddc82786c6dd61c6744378ad6
Ypuello <Yensi.Puello@theksquaregroup.com>
2022-07-01 14:57:46 -0400

    ...

--- build/run_sfdx_scanner.sh
+++ build/run_sfdx_scanner.sh
@@ -1,7 +1,7 @@
 echo "Installing JDK"
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt-get update
-sudo apt-get install openjdk-8-jdk
+sudo apt-get install openjdk-8-jre
 
 echo "Install SFDX Scanner"
 echo -e 'y/n' | sfdx plugins:install @salesforce/sfdx-scanner
