8f0fa54061a3e4395d6c868b199185f808d92c63
Ypuello <Yensi.Puello@theksquaregroup.com>
2022-07-07 11:46:09 -0400

    ...

--- build/deploy_code.sh
+++ build/deploy_code.sh
@@ -2,4 +2,4 @@
 #sfdx force:source:convert -d deploy_code -r force-app
 
 echo "Deploying code to org"
-sfdx force:mdapi:deploy -u DevHub -d deploy_code/ -w -1 -l RunSpecifiedTests --runtests AccountTriggerHandlerTest 
+sfdx force:mdapi:deploy -u DevHub force-app -w -1 -l RunSpecifiedTests --runtests AccountTriggerHandlerTest 
